18acba6d6435334b4bd0d19ac357c69199184f70
Daniel Radtke <daniel.radtke@gmail.com>
2016-10-22 17:26:52 -0500

    Initial Code Commit

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace uTikDownloadHelper
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            OpenFileDialog ofdTik = new OpenFileDialog();
+            ofdTik.Filter = ".tik File|*.tik";
+            string ticketInPath;
+            if (args.Length != 1) {
+                if (ofdTik.ShowDialog() == DialogResult.OK)
+                {
+                    ticketInPath = ofdTik.FileName;
+                }
+                else
+                    return;
+            } else
+            {
+                ticketInPath = args[0];
+            }
+
+            Byte[] ticket = File.ReadAllBytes(ticketInPath);
+            patchTicket(ref ticket);
+            string hexID = "";
+
+            for (int i = 0x1DC; i < 0x1DC + 8; i++)
+            {
+                hexID += string.Format("{0:X2}", ticket[i]);
+            }
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select the folder to download the files to.";
+
+            ChooseFolder:
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                int directoryItems = 0;
+                directoryItems += Directory.GetFiles(fbd.SelectedPath).Length;
+                directoryItems += Directory.GetDirectories(fbd.SelectedPath).Length;
+
+                string nusGrabberPath = Path.Combine(fbd.SelectedPath, "NUSgrabber.exe");
+                string wgetPath = Path.Combine(fbd.SelectedPath, "wget.exe");
+                string vcRuntime140Path = Path.Combine(fbd.SelectedPath, "vcruntime140.dll");
+
+                if (directoryItems == 0)
+                {
+                    File.WriteAllBytes(nusGrabberPath, uTikDownloadHelper.Properties.Resources.NUSgrabber);
+                    File.WriteAllBytes(wgetPath, uTikDownloadHelper.Properties.Resources.wget);
+                    File.WriteAllBytes(vcRuntime140Path, uTikDownloadHelper.Properties.Resources.vcruntime140);
+
+                    var procStIfo = new ProcessStartInfo(nusGrabberPath, hexID);
+                    procStIfo.RedirectStandardOutput = false;
+                    procStIfo.UseShellExecute = false;
+                    procStIfo.CreateNoWindow = false;
+                    procStIfo.WorkingDirectory = fbd.SelectedPath;
+
+                    var proc = new Process();
+                    proc.StartInfo = procStIfo;
+                    proc.Start();
+
+                    proc.WaitForExit();
+
+                    File.Delete(nusGrabberPath);
+                    File.Delete(wgetPath);
+                    File.Delete(vcRuntime140Path);
+
+                    string downloadPath = fbd.SelectedPath + "\\" + hexID;
+                    if (Directory.Exists(downloadPath))
+                    {
+                        foreach (string file in Directory.GetFiles(downloadPath))
+                        {
+                            string name = Path.GetFileName(file);
+
+                            File.Move(file, Path.Combine(fbd.SelectedPath, name));
+                        }
+                        Directory.Delete(downloadPath);
+                    }
+
+                    File.WriteAllBytes(Path.Combine(fbd.SelectedPath, "title.tik"), ticket);
+
+                } else
+                {
+                    if (MessageBox.Show("The selected folder isn't empty, do you want to choose another folder?", "Folder Not Empty",MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        goto ChooseFolder;
+                    }
+                }
+            }
+        }
+
+        static void patchTicket(ref Byte[] bytes)
+        {
+            if (bytes[0x01] == 0x3)
+            {
+                bytes[0x1] = 1;
+                bytes[0xF] = (byte)((int)bytes[0xF] ^ 2);
+            }
+        }
+
+        static void writeBytes(Byte[] bytes, Stream output)
+        {
+            output.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# uTikDownloadHelper
+
+## About
+This utility is meant to simply patching a Wii U .tik file and downloading the title data using NUSgrabber.
+
+## Usage
+Run the application, choose the .tik file, and choose a destination folder that is empty.
+
+You can also drag and drop a .tik file onto the exe instead.
+
+## Credits
+* Crediar for NUSgrabber
+* The Brazilian team that figured out how to patch the .tik files
